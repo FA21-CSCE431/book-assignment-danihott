1a0dcd020efea06e91c520a6af19611b6280af26
Dani Hott <daniel.hott@tamu.edu>
2021-09-25 14:54:42 -0500

    LinkedIn tutorial

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'demo/index'
+  get 'demo/hello'
   resources :books
   root 'books#index'
+  #get ':controller(/:action(/:id))'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
